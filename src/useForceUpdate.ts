36fde74ec648f6822e7c4c2a980f2e8c4effa77a
Jason Walton <jwalton@solinkcorp.com>
2021-03-17 16:38:25 -0400

    fix: Fix broken `useForceUpdate`.

--- src/useForceUpdate.ts
+++ src/useForceUpdate.ts
@@ -1,14 +1,15 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export function useForceUpdate(): () => void {
-    const count = useRef(0);
     const [, setCount] = useState(0);
 
     return useCallback(() => {
-        if (count.current === Number.MAX_SAFE_INTEGER) {
-            setCount(0);
-        } else {
-            setCount(count.current + 1);
-        }
+        setCount((count) => {
+            if (count === Number.MAX_SAFE_INTEGER) {
+                return 0;
+            } else {
+                return count + 1;
+            }
+        });
     }, []);
 }
