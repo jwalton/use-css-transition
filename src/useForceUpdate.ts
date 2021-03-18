6b4e3d794122d1e12822ce5827f74133f5f13020
Jason Walton <jwalton@solinkcorp.com>
2021-03-18 17:29:00 -0400

    fix: Fix case where "from" state correctly being applied when item first appears.

--- src/useForceUpdate.ts
+++ src/useForceUpdate.ts
@@ -3,13 +3,23 @@ import { useCallback, useState } from 'react';
 export function useForceUpdate(): () => void {
     const [, setCount] = useState(0);
 
+    let defer: (fn: () => void) => void;
+
+    if (typeof window !== undefined) {
+        defer = window.requestAnimationFrame;
+    } else {
+        defer = (fn: () => void) => setTimeout(fn, 1);
+    }
+
     return useCallback(() => {
-        setCount((count) => {
-            if (count === Number.MAX_SAFE_INTEGER) {
-                return 0;
-            } else {
-                return count + 1;
-            }
+        defer(() => {
+            setCount((count) => {
+                if (count === Number.MAX_SAFE_INTEGER) {
+                    return 0;
+                } else {
+                    return count + 1;
+                }
+            });
         });
     }, []);
 }
